9f4cc67e1b9a268c02dcd7a915bcedc223793bda
Ahmad Shafwan Dzaky <99414654+AhmadShafwanDzaky@users.noreply.github.com>
2023-10-18 17:43:34 +0800

    fix: make the photo more rounded and bold the name text

--- app/src/main/java/com/example/nyoba_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/nyoba_kotlin/MainActivity.kt
@@ -10,13 +10,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -45,19 +48,20 @@ fun Greeting(fullName: String, title: String, wa: String, ig: String, dribbble: 
     Column (
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(horizontal = 12.dp)
+        modifier = modifier.padding(horizontal = 64.dp)
     ){
         Image(
             painter = painterResource(id = R.drawable.pp),
             contentDescription = null,
             modifier = Modifier
                 .size(100.dp)
-                .padding(bottom = 12.dp))
+                .clip(RoundedCornerShape(24.dp)))
 
         Text(
             text = fullName,
-            modifier = modifier,
+            modifier = Modifier.padding(top = 20.dp),
             fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
             textAlign = TextAlign.Center
         )
         Text(
